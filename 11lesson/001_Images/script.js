71697386173bba0688b7baa10f53003d6bf79fc8
glubokij1985 <glubokij85@gmail.com>
2016-05-15 18:19:57 +0300

    searchProp

---
+++ 11lesson/001_Images/script.js
@@ -0,0 +1,10 @@
+function preload(images) {
+        var i = 0;
+        var imageArray = new Array();
+        imageArray = images.split(',');
+        var imageObj = new Image();
+        for ( i=0; i <= imageArray.length-1; i++) {
+            document.write('<img src="' + imageArray[i] + '" />');
+            imageObj.src = imageArray[i];
+        };
+};
